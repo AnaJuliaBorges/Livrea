920cbbdd7a556db9721ae85a5f68227a32455efc
Ana Julia Borges <najuborgess@gmail.com>
2026-07-01 21:19:57 -0300

    feat: add empty state my clubs

--- src/features/clubs/pages/myClubs.tsx
+++ src/features/clubs/pages/myClubs.tsx
@@ -5,6 +5,7 @@ import { Separator } from "@/components/ui/separator";
 import { Button } from "@/components/ui/button";
 import { useNavigate } from "react-router-dom";
 import { myClubs } from "@/mocks/clubes";
+import { Search } from "lucide-react";
 
 export default function MyClubs() {
   const navigate = useNavigate();
@@ -21,16 +22,27 @@ export default function MyClubs() {
         />
       </div>
 
-      <p className="font-medium">Meus clubes</p>
+      {myClubs.length === 0 ? (
+        <div className="flex flex-col h-[70vh] justify-center items-center text-center gap-5">
+          <Search className="inline-block text-gray-300" size={86} />
+          Você ainda não está em nenhum clube. <br />
+          Crie um clube ou entre em um disponível.
+        </div>
+      ) : (
+        <>
+          <p className="font-medium">Meus clubes</p>
 
-      <div>
-        {myClubs?.map((club: Club) => (
-          <>
-            <ItemClub key={club.id} club={club} />
-            <Separator className="my-4" />
-          </>
-        ))}
-      </div>
+          <div>
+            {myClubs?.map((club: Club) => (
+              <>
+                <ItemClub key={club.id} club={club} />
+                <Separator className="my-4" />
+              </>
+            ))}
+          </div>
+        </>
+      )}
+
       <div className="fixed bottom-24 left-0 right-0 z-20 w-full">
         <div className="mx-auto max-w-3xl px-4">
           <Button
